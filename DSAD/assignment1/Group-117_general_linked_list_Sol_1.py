019da333bbc79c651993fc7d8e2016bda1fc174d
Prabhanjan Reddy <prabhanjan.215@gmail.com>
2021-12-22 11:24:13 +0530

    updated design doc

--- DSAD/assignment1/Group-117_general_linked_list_Sol_1.py
+++ DSAD/assignment1/Group-117_general_linked_list_Sol_1.py
@@ -1,203 +1,208 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 15 2021 19:40:29 2020
-@Program: BITS WILP SEM_1 DSAD Assignment 1
-@author: Sai Prabhanjan Reddy, Vinayak Nayak and Niyati Aagarwal
-@Section: Sec-2
-@Group: 117
-"""
-import time
+'''
+@author Vinayak, Sai, Niyati
+@email 2021fc04135@wilp.bits-pilani.ac.in
+@create date 2021-12-19 16:43:05
+@modify date 2021-12-21 23:30:31
+@desc [Code to implement company acquisition in the form of a general tree using linked lists]
+'''
 
+from typing import List
+from pathlib import Path
+import re
 
-class LinkedTreeNode:
-    """
-    A General Tree linked list structure, Kept minimal to parent child relationship with no weights
-    child_list: list of children for searched node
-    company_name: input company name
-    basic_tree: initial tree with company root node (boolean)
-    """
-    child_list = []
-    company_name = None
-    basic_tree = None
+class Node:
+    def __init__(self, parent_company: object, company_name: str):
+        """[Create a node object given the company's name and it's parent company]
 
-    def __init__(self, company_name):
+        Args:
+            parent_company (str): [Name of the parent company]
+            company_name (str): [Name of this company]
+        """
+
         self.company_name = company_name
-        self.child_list = []
+        self.parent_company = parent_company
+        
+        # Maintain a list of companies acquired by this company
+        self.acquired_companies = []
 
-    def add_node(self, node):
-        # Add a preceding node
-        self.child_list.append(node)
+class Tree:
+    def __init__(self, root: Node):
+        """[Create a tree data structure to hold the organization hierarchy]
 
-    def remove_node(self, node):
-        # Remove the searched node
-        self.child_list.remove(node)
-
-    def find_child_and_parent(self, child_company, parent_company=None):
-        # Get the node and parent
-        if self.company_name == child_company:
-            return self, parent_company
-        for child in self.child_list:
-            child_node, parent_company = child.find_child_and_parent(child_company, self)
-            if child_node is not None:
-                return child_node, parent_company
-        return None, None
-
-
-class InputParser:
-    """
-    This class is defined to parse the input file from given structure and call the required operations
-    of a tree
-    """
-    input_lines = []
-    list_ops = []
-    write_out = None
-
-    # Operations Naming Keywords
-    add_root_node = "Company:"
-    ops_count = "No of operations:"
-    operation_acquire = "ACQUIRED"
-    operation_detail = "DETAIL"
-    operation_release = "RELEASE"
-
-    def __init__(self, input_file="inputPS5.txt", output_file="outputPS5.txt"):
+        Args:
+            root (Node): [The root node/ super parent company]
         """
-        Takes the input file and parses it line by line and executes the required operations based on the
-        listed keywords as mentioned in the below code.
-        :param input_file: Set of instructions regarding Company general tree of operations
-        :param output_file: Status of each operation from input list
+        self.root = root
+    
+    def search(self, company_name: str) -> List:
+        """[Searches for a given company in the organization structure and returns the same using BFS]
+
+        Args:
+            company_name (str): [Name of the company to be searched]
+
+        Returns:
+            List: [boolean: whether or not found, Node/String: Node if found, empty string if not found]
+        """
+        
+        # Create a queue for traversing the tree
+        search_queue = []
+        
+        # Load the root in the queue to start processing
+        search_queue.append(self.root)
+        n = len(search_queue)
+
+        while n > 0:
+            # Pop the first element from the front
+            front = search_queue.pop(0)
+
+            # Check if this node is the company we're looking for
+            if front.company_name == company_name:
+                return [True, front]
+            
+            # Otherwise load the current company's subsidiaries into the queue
+            for subsidiary in front.acquired_companies:
+                search_queue.append(subsidiary)
+            
+            n = len(search_queue)
+
+        return [False, "Requested company doesn't exist"]
+    
+    def acquire(self, parent_company: str, acquired_company: str) -> str:
+        """[Given a parent and subsidiary company, add the subsidiary company to the list of companies acquired by parent company]
+
+        Args:
+            parent_company (str): [Name of the parent company]
+            acquired_company (str): [Name of the acquired company]
+
+        Returns:
+            str: [Status of the acquisition]
         """
 
-        self.list_ops = []
-        self.input_lines = []
-        try:
-            with open(input_file) as f:
-                self.input_lines = f.readlines()
-                f.close()
-                # print(self.lines)
-        except EnvironmentError:
-            print("Failed to read input_file")
+        if self.root is None:
+            return "No company data available"
+        
+        presence = self.search(parent_company)
+        acquired_presence = self.search(acquired_company)
+        # Check if the company which is acquiring (parent company) is already present in the hierarchy or not
+        if not presence[0]:
+            return f"ACQUIRED FAILED: {acquired_company} by {parent_company}"
+        # Check if the company that is to be acquired is already in the hierarchy
+        elif acquired_presence[0]:
+            return f"ACQUIRED FAILED:{acquired_company} BY:{parent_company}"
+        # Otherwise, add the acquired company as a subsidiary of the parent company
+        else:
+            subsidiary_company = Node(presence[1], acquired_company)
+            presence[1].acquired_companies.append(subsidiary_company)
+            return f"ACQUIRED SUCCESS: {acquired_company} BY:{parent_company}"
+    
+    def detail(self, company_name: str) -> str:
+        """[Given a company's name, prints out the details of that company]
 
-        try:
-            InputParser.write_out = open(output_file, "w")
-        except EnvironmentError:
-            print("Failed to open file in write mode", input_file)
+        Args:
+            company_name (str): [Name of the company]
 
-    def execute_input(self):
+        Returns:
+            str: [Details of the company]
         """
-        Function to execute the instructions received from input and keywords as mentioned in parser function
-        :return: executes the operations as per directive and exits if invalid input is supplied
-        """
-        # print(self.lines)
-        if len(self.input_lines) > 0:
-            for _l in self.input_lines:
-                # print(line)
-                task = _l.split()
-                # print(self.operations)
-                # print(task)
-                # print(cmd, len(cmd))
-                if len(task) == 2:
-                    # Root node
-                    if task[0].lower() == self.add_root_node.lower():
-                        LinkedTreeNode.basic_tree = LinkedTreeNode(task[1])
-                    if task[0].lower() == self.operation_detail.lower():
-                        detail(task[1])
-                    if task[0].lower() == self.operation_release.lower():
-                        release(task[1])
-                    if task[0].split(":")[0].lower() == self.operation_acquire.lower():
-                        child = task[0].split(":")[-1]
-                        parent = task[1].split(":")[-1]
-                        acquire(parent, child)
+        
+        if self.root is None:
+            return "No company data available"
+
+        presence = self.search(company_name)
+        
+        # Check if the company requested for exists in the organization hierarchy
+        # If not, then print detail failed
+        if not presence[0]:
+            return f"DETAIL: {company_name}\nDETAIL FAILED: {company_name} does not exist in the organizational hierarchy"
+        
         else:
-            print("Input file is Invalid, Please check again")
-            exit()
+            # Get the node for the company
+            company = presence[1]
 
-    def Print(str):
-        if InputParser.write_out is None:
-            print(str)
-        else:
-            print(str, file=InputParser.write_out)
-
-
-def detail(company_name):
-    """
-    this function prints the parent and immediate children of company
-    :param company_name: input company name to get the details
-    :return: Company details such as list of child company names and count
-    """
-    if LinkedTreeNode.basic_tree is None:
-        InputParser.Print("No Company information available")
-    searched_node, parent = LinkedTreeNode.basic_tree.find_child_and_parent(company_name)
-    if searched_node is not None:
-        InputParser.Print("DETAIL: {0}".format(searched_node.company_name))
-        if len(searched_node.child_list):
-            children = [child.company_name for child in searched_node.child_list]
-            # InputParser.Print("Parent Company: {0}".format(parent.company_name))
-            InputParser.Print("Acquired companies: {0}".format(",".join(children)))
-        else:
-            # InputParser.Print("Parent Company: {0}".format(parent.company_name))
-            InputParser.Print("Acquired companies: none")
-        InputParser.Print("No of companies acquired: {0}".format(len(searched_node.child_list)))
-    else:
-        InputParser.Print("Company does not exist")
-
-
-def acquire(parent_company, acquired_company):
-    """
-     Inserts the acquired_company as a new child node to the parent_company
-    :param parent_company: The name of the company to which the child needs to be added
-    :param acquired_company: The acquired company name
-    :return: Prints the status of Acquisition as Success or Failure if no mentioned parent available
-    """
-
-    if LinkedTreeNode.basic_tree is None:
-        InputParser.Print("No Company data exist")
-
-    searched_node, parent = LinkedTreeNode.basic_tree.find_child_and_parent(acquired_company)
-
-    if parent is not None:
-        InputParser.Print("ACQUIRED FAILED: {0} BY:{1}".format(acquired_company, parent_company))
-        return
-
-    searched_node, parent = LinkedTreeNode.basic_tree.find_child_and_parent(parent_company)
-
-    if searched_node is not None:
-        searched_node.add_node(LinkedTreeNode(acquired_company))
-        InputParser.Print("ACQUIRED SUCCESS:{1} Successfully acquired {0}".format(acquired_company, searched_node.company_name))
-    else:
-        InputParser.Print("ACQUIRED FAILED:{0} BY:{1}".format(acquired_company, parent_company))
-
-
-def release(released_company):
-    """
-    removes the node mentioned in the released_company.
-    :param released_company: The company name which to be removed
-    :return: Prints the status of release as Success or Failure if no mentioned parent available
-    """
-
-    if LinkedTreeNode.basic_tree is None:
-        InputParser.Print("No Company data exist")
-
-    search_node, parent = LinkedTreeNode.basic_tree.find_child_and_parent(released_company)
-    # Checking for node existance
-    if search_node is not None:
-        if parent is None:  # Root node
-            LinkedTreeNode.basic_tree = None
-        else:
-            if len(search_node.child_list) > 0:
-                for _child in search_node.child_list:
-                    parent.add_node(LinkedTreeNode(_child.company_name))
-                    # InputParser.Print("Child stitched Company: {0}".format(parent.company_name))
-                parent.remove_node(search_node)
+            # Construct the detail string as per the output format
+            details_string =  f"DETAIL: {company_name}"
+            subsidiaries = [x.company_name for x in company.acquired_companies]
+            if len(subsidiaries):
+                details_string += f"\nAcquired companies: {', '.join(subsidiaries)}"
+                details_string += f"\nNo of companies acquired: {len(subsidiaries)}"
             else:
-                parent.remove_node(search_node)
-            InputParser.Print("RELEASED SUCCESS: released {0} successfully.".format(search_node.company_name))
-    else:
-        InputParser.Print("RELEASED FAILED: released {0} failed".format(released_company))
+                details_string += f"\nAcquired companies: none\nNo of companies acquired: 0"
+                
+        return details_string
+    
+    def release(self, company_name: str) -> str:
+        """[Given a company name, releases the company from the organizational hierarchy]
 
+        Args:
+            company_name (str): [Name of the company to be removed]
 
-if __name__ == "__main__":
-    stat = time.time()
-    gTree = InputParser()
-    gTree.execute_input()
-    gTree.write_out.close()
-    print(time.time()-stat)
+        Returns:
+            str: [Status of the removal operation]
+        """
+        if self.root is None:
+            return "No company data available"
+
+        presence = self.search(company_name)
+
+        # Check if the requested company to release exists in the organizational hierarchy
+        if not presence[0]:
+            return f"RELEASE FAILED: release {company_name} failed."
+        # Check if the requested company is the master company, if so raise an exception
+        else:
+            to_remove = presence[1]
+            if self.root.company_name == company_name:
+                self.root = None
+                return f"RELEASE SUCCESS: released {company_name} succesfully."
+            else:
+                # Create an empty list for new subsidiaries (devoid of this company)
+                new_subsidiaries = []
+                
+                # In this company's parent, find the mention of this company and remove it
+                for subsidiary in to_remove.parent_company.acquired_companies:
+                    if not(subsidiary.company_name == company_name):
+                        new_subsidiaries.append(subsidiary)
+                    else:
+                        for grandson_subsidiary in to_remove.acquired_companies:
+                            grandson_subsidiary.parent_company = to_remove.parent_company
+                            new_subsidiaries.append(grandson_subsidiary)
+
+                to_remove.parent_company.acquired_companies = new_subsidiaries
+            return f"RELEASE SUCCESS: released {company_name} successfully."
+            
+def parse_input(input_pth: str):
+    """[Reads an input file, performs the operations in it in a line by line fashion and prints the output to another file]
+    """  
+    # Strip leading and trailing whitespaces from instructions and read it
+    instructions = [x.strip() for x in Path(input_pth).read_text().split("\n")]
+    
+    # Open an output file and start logging everything over there
+    with open("output.txt", "w") as f:
+        # Read the base company name and number of operations
+        base_conglomerate = instructions[0].replace("Company: ", "")
+        n = int(re.findall(r"\d+", instructions[1])[0])
+        f.writelines(f"Company: {base_conglomerate}\nNo of operations: {n}\n")
+        company_hierarchy = Tree(Node(None, base_conglomerate))
+            
+        # Start from the third line of instruction from the input text file and read next n lines
+        for instruction in instructions[2 : (n + 1)]:
+            try:
+                # Write the details of a company
+                if instruction.lower().startswith("detail"):
+                    company_name = instruction[7:]
+                    to_write = company_hierarchy.detail(company_name)
+                # Acquire a company and log it to the output file
+                elif instruction.lower().startswith("acquired"):
+                    instruction = instruction[9:]
+                    acquired_company = re.findall(r"(.+?) BY", instruction)[0]
+                    parent_company = re.findall(r"BY:(.+)", instruction)[0]
+                    to_write = company_hierarchy.acquire(parent_company, acquired_company)
+                # Release a company and log it to the output file
+                elif instruction.lower().startswith("release"):
+                    to_release = instruction[8:]
+                    to_write = company_hierarchy.release(to_release)
+            except Exception as e:
+                to_write = "ERROR: Couldn't interpret the instruction"
+
+            f.writelines(f"{to_write}\n")
+
+parse_input("sample_input.txt")                
