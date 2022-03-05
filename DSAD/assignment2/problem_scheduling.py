d9990e350df406b9431bfed6ddfcc9a481950472
Vinayak <vnayak@okkular.io>
2022-03-05 23:13:00 +0530

    Added partially complete design doc and some test cases prompts to test the working of algorithm

--- DSAD/assignment2/problem_scheduling.py
+++ DSAD/assignment2/problem_scheduling.py
@@ -2,13 +2,17 @@
 @author Vinayak
 @email vnayak@okkular.io / nayakvinayak95@gmail.com
 @create date 2022-03-04 21:06:47
-@modify date 2022-03-05 09:20:40
-@desc [Job Scheduling algorithm]
+@modify date 2022-03-05 23:12:24
+@desc [Problem Scheduling algorithm]
 '''
 
-import sched
 from typing import List, Tuple
 from pathlib import Path
+
+# Define two global variables one for indicating if we need to finish all problems
+# And another to tell if the order of processes needs to be mentioned
+SCHEDULE_ALL = False
+SCHEDULE_ORDER = True
 
 class problem:
     def __init__(self, name:str, deadline:int, bonus:int):
@@ -89,7 +93,7 @@ def custom_sort(problem_list:List, attribute:str, descending:bool = True):
             j += 1
             k += 1
 
-def schedule_problems(problem_list:List, attribute:str) -> Tuple:
+def schedule_problems(problem_list:List, attribute:str, schedule_all:bool = False) -> Tuple:
     """[Given a list of problems, schedules them to maximize the bonus]
 
     Args:
@@ -106,7 +110,7 @@ def schedule_problems(problem_list:List, attribute:str) -> Tuple:
     total_problems = len(problem_list)
 
     # Generate a slots array for scheduling the best problems
-    slots = ["empty"] * total_problems
+    slots = [""] * total_problems
 
     # Accumulate bonus in a variable, initialize it to zero
     max_bonus = 0
@@ -119,25 +123,26 @@ def schedule_problems(problem_list:List, attribute:str) -> Tuple:
         # Look for the latest time slot for this problem which is not empty
         for idx in range(min(deadline - 1, total_problems),-1,-1):
             slt = slots[idx]
-            if slt == "empty":
+            if slt == "":
                 slots[idx] = problem.name
                 max_bonus += bonus
                 break
     
-    # Look at the problems which haven't been scheduled so far
-    # They can be scheduled in any order but it makes sense to schedule 
-    # them in the ascending order of their deadlines from a practical point of view
-    unscheduled_problems = []
-    for problem in problem_list:
-        if not problem.name in slots:
-            unscheduled_problems.append(problem)
-    custom_sort(unscheduled_problems, "deadline", descending = False)
+    if schedule_all:
+        # Look at the problems which haven't been scheduled so far
+        # They can be scheduled in any order but it makes sense to schedule 
+        # them in the ascending order of their deadlines from a practical point of view
+        unscheduled_problems = []
+        for problem in problem_list:
+            if not problem.name in slots:
+                unscheduled_problems.append(problem)
+        custom_sort(unscheduled_problems, "deadline", descending = False)
 
-    unscheduled_problems_counter = 0
-    for idx in range(total_problems):
-        if slots[idx] == "empty":
-            slots[idx] = unscheduled_problems[unscheduled_problems_counter].name
-            unscheduled_problems_counter += 1            
+        unscheduled_problems_counter = 0
+        for idx in range(total_problems):
+            if slots[idx] == "":
+                slots[idx] = unscheduled_problems[unscheduled_problems_counter].name
+                unscheduled_problems_counter += 1            
             
     return (slots, max_bonus)
 
@@ -149,23 +154,39 @@ if __name__ == "__main__":
     inputs = [x.replace("\n", "") for x in Path("inputPS5.txt").read_text().splitlines()]
     
     # Infer the number of test cases from the file
-    n_test_cases = int(inputs[0].replace("No of use-cases:", "").strip())
+    n_test_cases = int(inputs[0].split(":")[-1].strip())
+
+    # Create two containers to hold the bonuses and sequence of completion for each problem
+    final_bonuses = []
+    final_sequences = []
 
     for c in range(n_test_cases):
         # Read the deadlines and bonuses for the cth test case
-        deadlines = inputs[1 + 2*c]
-        bonuses = inputs[(c + 1) * 2]
-
-        deadlines = [int(x.strip()) for x in deadlines.replace("Deadlines: ", "").split(" ")]
-        bonuses = [int(x.strip()) for x in bonuses.replace("Bonus: ", "").split(" ")]
+        deadlines = inputs[1 + 2*c].split(":")[-1].strip()
+        bonuses = inputs[(c + 1) * 2].split(":")[-1].strip()
+        
+        deadlines = [int(x.strip()) for x in deadlines.split(" ")]
+        bonuses = [int(x.strip()) for x in bonuses.split(" ")]
 
         # Iterate over the deadlines and bonuses and create a problem list for this test case
         problem_list = []
         for idx, (deadline, bonus) in enumerate(zip(deadlines, bonuses)):
-            name = chr(65 + idx)
+            name = f"Problem_{idx + 1}"
             pb = problem(name, deadline, bonus)
             problem_list.append(pb)
         
-        slots, bonus = schedule_problems(problem_list, "bonus")
-        print(slots, bonus)
-        
+        # Run the scheduling algorithm and accumulate the sequence of problems and the bonus accrued
+        slots, bonus = schedule_problems(problem_list, "bonus", schedule_all = SCHEDULE_ALL)
+        final_bonuses.append(bonus)
+        final_sequences.append(slots)
+    
+    # Log the bonuses to an output file
+    with open("outputPS5.txt", "w") as f:
+        for bonus in final_bonuses:
+            f.writelines(f"{bonus}\n")
+    
+    # Log the order in which problems are scheduled if asked
+    if SCHEDULE_ORDER:
+        with open("output_with_order.txt", "w") as f:
+            for idx, (sequence, bonus) in enumerate(zip(final_sequences, final_bonuses), start = 1):
+                f.writelines(f"Test case: {idx:2d} Bonus: {bonus:4d} Job Sequence: {str(sequence):<20}\n")
